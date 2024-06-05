e84c9bf9125cc0b4bc0b0fa89a8e905e8f59f0e4
Gibet13 <48248176+Gibet13@users.noreply.github.com>
2024-06-05 14:35:19 +0200

    api platform

--- src/Entity/Commentaire.php
+++ src/Entity/Commentaire.php
@@ -2,10 +2,12 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\CommentaireRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
+#[ApiResource]
 #[ORM\Entity(repositoryClass: CommentaireRepository::class)]
 class Commentaire
 {
